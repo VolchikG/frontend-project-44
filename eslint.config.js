f455709debc3e3631b4a2c68010f911b89c15fb1
Volchik <nik5102006@gmail.com>
2025-06-10 09:38:25 +0300

    fix lint navernoe

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,7 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
 
-
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
-]);
-
-
+export default [
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+];
