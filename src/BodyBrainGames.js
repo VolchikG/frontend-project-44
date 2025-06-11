1b7cde1715f4c5c90ee69ab9d6b4f23b1afc7952
unknown <nik5102006@gmail.com>
2025-06-11 12:47:55 +0300

    fix

--- src/BodyBrainGames.js
+++ src/BodyBrainGames.js
@@ -5,7 +5,7 @@ const BodyBrainGames = (description, taskFunc) => {
   const playerName = StartBG()
   console.log(description)
   let mark = 0
-  
+
   for (let i = 1; i <= 3; i += 1) {
     const step = taskFunc()
 
@@ -21,7 +21,6 @@ const BodyBrainGames = (description, taskFunc) => {
       break
     }
   }
-
   if (mark === 3) {
     console.log(`Congratulations, ${playerName}!`)
   }
