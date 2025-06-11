964d3fc2e4d87453356c55cfa52a0d84aa7e0269
unknown <nik5102006@gmail.com>
2025-06-11 13:03:25 +0300

    ese fix

--- src/games/even.js
+++ src/games/even.js
@@ -1,30 +1,14 @@
-import readlineSync from 'readline-sync'
-import StartBG from './cli.js'
+const BrainEven = () => {
+  const q = Math.round(Math.random() * 100)
+  let a = ''
 
-const BodyBrainGames = (description, taskFunc) => {
-  const playerName = StartBG()
-  console.log(description)
-  let mark = 0
-
-  for (let i = 1; i <= 3; i += 1) {
-    const step = taskFunc()
-
-    console.log(`Question: ${step[0]}`)
-    const answer = readlineSync.question('Answer: ')
-
-    if (answer === step[1]) {
-      console.log('Correct!')
-      mark += 1
-    } else {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${step[1]}'.`)
-      console.log(`Let's try again, ${playerName}!\n`)
-      break
-    }
+  if (q % 2 === 0) {
+    a = 'yes'
+  } else {
+    a = 'no'
   }
 
-  if (mark === 3) {
-    console.log(`Congratulations, ${playerName}!`)
-  }
+  return [q, a]
 }
 
-export default BodyBrainGames
+export default BrainEven
