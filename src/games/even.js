bec669e476cfe210cba6e0f124c6ebdc608862e1
unknown <nik5102006@gmail.com>
2025-06-11 13:00:41 +0300

    fix fiksa

--- src/games/even.js
+++ src/games/even.js
@@ -1,14 +1,30 @@
-const BrainEven = () => {
-  const q = Math.round(Math.random() * 100)
-  let a = ''
+import readlineSync from 'readline-sync'
+import StartBG from './cli.js'
 
-  if (q % 2 === 0) {
-    a = 'yes'
-  } else {
-    a = 'no'
+const BodyBrainGames = (description, taskFunc) => {
+  const playerName = StartBG()
+  console.log(description)
+  let mark = 0
+
+  for (let i = 1; i <= 3; i += 1) {
+    const step = taskFunc()
+
+    console.log(`Question: ${step[0]}`)
+    const answer = readlineSync.question('Answer: ')
+
+    if (answer === step[1]) {
+      console.log('Correct!')
+      mark += 1
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${step[1]}'.`)
+      console.log(`Let's try again, ${playerName}!\n`)
+      break
+    }
   }
 
-  return [q, a]
+  if (mark === 3) {
+    console.log(`Congratulations, ${playerName}!`)
+  }
 }
 
-export default BrainEven
+export default BodyBrainGames
